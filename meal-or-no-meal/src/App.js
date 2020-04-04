28a266c667dfe36e5017ccb2c41ba7025030b74a
Trevor Moser <trevor.moser28@gmail.com>
2020-04-04 11:51:39 -0500

    Created new react app and replaced with meal or no meal logo. Added first integration test.

---
+++ meal-or-no-meal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App = () => {
+  return (
+    <div>
+      <img data-testid="logo" src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
