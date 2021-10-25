a3332e92f1c711aae89b4bd4bdc48224bdbc9a4e
Trevor Moser <trevor.moser@e-hps.com>
2021-10-25 16:03:40 -0500

    Changing to a HashRouter in attempt to work with gh pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Layout from "./layout";
 import Recipes from "./pages/recipes";
