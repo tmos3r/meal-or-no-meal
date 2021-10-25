927aa5b4b7a17a0dda9361b2f7184ee5fbf61e10
Trevor Moser <trevor.moser@e-hps.com>
2021-10-24 20:34:36 -0500

    New react app

--- meal-or-no-meal/src/layout/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-    primary: '#52b3b6',
-    secondary: '#f26249',
-    darkSecondary: '#ec2f3b',
-    lightSecondary: '#ff9b5a',
-    lighterSecondary: '#ffd58e'
-}
-
-export default colors;
